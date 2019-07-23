7e3e44ac797f2baac00d3a880c37eb62587acbde
Adhiv Dhar <adhivd@gmail.com>
2019-07-23 01:08:26 -0700

    second section added, still TBD

--- src/Components/Home.js
+++ src/Components/Home.js
@@ -1,5 +1,7 @@
 import React from 'react';
 import logo from './../assets/free-v-square.png';
+import rocket from './../assets/rocket.png';
+
 import { Link } from "react-router-dom";
 
 import './../styles/App.css';
@@ -15,6 +17,13 @@ function Home() {
                 <h1>Empowering the brighest Berkeley founders to build great startups</h1>
                 <h2>Free Ventures is Cal’s leading pre-seed startup incubator</h2>
                 <Link to="apply" className="apply">Apply to Batch XII</Link>
+            </div>
+        </div>
+        <div className="overview">
+            <div className="container">
+                <div className="icon"></div>
+                <h2>The Free Ventures Accelerator</h2>
+
             </div>
         </div>
     </section>

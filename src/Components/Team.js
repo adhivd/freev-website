5f8884ca51d88b058219268cb5a11f5152b08787
Adhiv Dhar <adhivd@gmail.com>
2019-07-23 23:13:45 -0700

    margins

---
+++ src/Components/Team.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import logo from './../assets/free-v-square.png';
+import { Link } from "react-router-dom";
+
+import './../styles/App.css';
+import './../styles/Team.css';
+
+
+function Team() {
+  return (
+    <section>
+
+    </section>
+  );
+}
+
+export default Team;

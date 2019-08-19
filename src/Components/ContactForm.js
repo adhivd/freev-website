8f68556094bdcc07832c9b76966fc14e242ea739
Adhiv Dhar <adhivd@gmail.com>
2019-08-19 00:22:15 -0700

    form round 3

--- src/Components/ContactForm.js
+++ src/Components/ContactForm.js
@@ -6,25 +6,30 @@ import React from 'react';
 import './../styles/App.css';
 import './../styles/Contact.css';
 
+const encode = (data) => {
+return Object.keys(data)
+    .map(key => encodeURIComponent(key) + "=" + encodeURIComponent(data[key]))
+    .join("&");
+}
+
 class ContactForm extends React.Component {
     constructor(props) {
       super(props);
       this.state = { name: "", email: "", message: "", company:"", subject: "" };
     }
 
-    encode = (data) => {
-    return Object.keys(data)
-        .map(key => encodeURIComponent(key) + "=" + encodeURIComponent(data[key]))
-        .join("&");
-  }
+
 
     handleSubmit = e => {
       fetch("/", {
         method: "POST",
         headers: { "Content-Type": "application/x-www-form-urlencoded" },
-        body: this.encode({ "form-name": "contact", ...this.state })
+        body: encode({ "form-name": "contact", ...this.state })
       })
-        .then(() => alert("Success!"))
+        .then(() => {
+            alert("Success!");
+            console.log("state: ", this.state);
+        })
         .catch(error => alert(error));
 
       e.preventDefault();
@@ -45,31 +50,42 @@ class ContactForm extends React.Component {
       }
 
       return (
-        <form data-netlify="true" data-netlify-honeypot="bot-field" onSubmit={this.handleSubmit} className="contact">
-            <input type="hidden" name="form-name" value="contact" />
-            <div className="two-grid" >
-                <p>
-                    <label>Your Name</label>
-                    <input type="text" name="name" value={name} onChange={this.handleChange} />
-                </p>
-                <p>
-                    <label>Your Email</label>
-                    <input type="email" name="email" value={email} onChange={this.handleChange} />
-                </p>
-            </div>
-          {companyConditional}
-          <p>
-            <label>Subject</label>
-            <input name="subject" value={subject} onChange={this.handleChange} />
-          </p>
-          <p>
-            <label>Message</label>
-            <textarea name="message" value={message} onChange={this.handleChange} />
-          </p>
-          <p style={{textAlign: 'center'}}>
-            <button className="genericButton" type="submit">Submit!</button>
-          </p>
-        </form>
+         <span>
+             <form name="contact" netlify netlify-honeypot="bot-field" hidden>
+               <input type="text" name="name" />
+               <input type="email" name="email" />
+               <input type="text" name="subject" />
+               <input type="text" name="company" />
+               <textarea name="message"></textarea>
+             </form>
+
+             <form data-netlify="true" data-netlify-honeypot="bot-field" onSubmit={this.handleSubmit} netlify="true" name="contact" className="contact">
+                 <input type="hidden" name="form-name" value="contact" />
+                 <div className="two-grid" >
+                     <p>
+                         <label>Your Name</label>
+                         <input type="text" name="name" value={name} onChange={this.handleChange} />
+                     </p>
+                     <p>
+                         <label>Your Email</label>
+                         <input type="email" name="email" value={email} onChange={this.handleChange} />
+                     </p>
+                 </div>
+               {companyConditional}
+               <p>
+                 <label>Subject</label>
+                 <input name="subject" value={subject} onChange={this.handleChange} />
+               </p>
+               <p>
+                 <label>Message</label>
+                 <textarea name="message" value={message} onChange={this.handleChange} />
+               </p>
+               <p style={{textAlign: 'center'}}>
+                 <button className="genericButton" type="submit">Submit!</button>
+               </p>
+             </form>
+         </span>
+
       );
     }
   }

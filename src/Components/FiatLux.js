0cae15b1b76e57ad1801445139ae42c03e96f000
Adhiv Dhar <adhivd@gmail.com>
2019-08-25 17:41:16 -0700

    small stuff

--- src/Components/FiatLux.js
+++ src/Components/FiatLux.js
@@ -2,6 +2,7 @@ import React from 'react';
 import './../styles/App.css';
 import GenericTop from './GenericTop';
 import GenericPage from './GenericPage';
+import ContactForm from './ContactForm';
 
 function FiatLux() {
 
@@ -12,6 +13,17 @@ function FiatLux() {
                 description={'Insert description here'}
             />
             <GenericPage>
+                <img src="" alt="Fiat Lux Image 1" />
+                <img src="" alt="Fiat Lux Image 2" />
+                <h2>Overview</h2>
+                <p>Overciew text</p>
+                <p>Overview additional text</p>
+                <h2>Get Involved</h2>
+                <p>Attending</p>
+                <p>If you are a student interested in attending Fiat Lux, send an email to <a href="mailto:aashi@freeventures.org">aashi@freeventures.org</a>.</p>
+                <p>Speaking</p>
+                <p>If you are an industry professional looking to get involved as a speaker or company sponsor pelase fill out the form below</p>
+                <ContactForm />
             </GenericPage>
         </section>
     );

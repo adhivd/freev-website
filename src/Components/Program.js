3ebb2348ff8f3ff464e7ee631b297efb15c41576
nikitaa78 <nikitaashok025@gmail.com>
2019-08-26 22:20:14 -0700

    final program page changes -nikita

--- src/Components/Program.js
+++ src/Components/Program.js
@@ -8,10 +8,10 @@ import './../styles/App.css';
 import './../styles/Program.css';
 
 import freevgroup from './../assets/program/freevgroup.jpg'
-import thiel from './../assets/program/thiel.jpg';
+import thiel from './../assets/program/thiel.png';
 import forbes from './../assets/program/forbes.png';
 import yc from './../assets/program/yc.png';
-import greylock from './../assets/program/greylock.jpeg';
+import greylock from './../assets/program/greylock.png';
 import accel from './../assets/program/accel.png';
 import kp from './../assets/program/kp.png';
 
@@ -22,26 +22,29 @@ import community from './../assets/program/group.png';
 import legal from './../assets/program/banking.png';
 import fundraising from './../assets/program/fundraise.png';
 
-import method from './../assets/program/method.png';
-import intro from './../assets/program/intro.jpg';
-import spotlight from './../assets/program/spotlight.png';
+import method from './../assets/program/method.jpeg';
+import intro from './../assets/program/method.png';
+import spotlight from './../assets/program/spotlight.jpeg';
 
 function Program() {
 
     let articles = [{
         image: intro,
-        headline: "The Free Ventures Method",
-        date: "March 7, 2017"
+        headline: "Introducing Free Ventures’ Batch XII",
+        date: "May 3, 2019",
+        url: "https://medium.com/@FreeVentures/introducing-free-ventures-batch-xii-c9dd2fd3d25a"
     },
     {
         image: method,
-        headline: "Introducing Free Ventures' Spring '17 Batch & Trends in Berkeley Startups!",
-        date: "February 28, 2017"
+        headline: "Introducing Free Ventures’ Batch XI",
+        date: "November 30, 2018",
+        url: "https://medium.com/@FreeVentures/introducing-free-ventures-batch-xi-49e8fe433386"
     },
     {
         image: spotlight,
-        headline: "Founder Spotlight: Iris BCI, Free Ventures Fall '16 Batch",
-        date: "January 29, 2017"
+        headline: "Introducing COBA — The Coffee Bar",
+        date: "October 31, 2018",
+        url: "https://medium.com/@FreeVentures/introducing-coba-the-coffee-bar-4a02a4be5472"
     }
     ];
 
@@ -50,6 +53,7 @@ function Program() {
                 image={art.image}
                 headline={art.headline}
                 date={art.date}
+                url={art.url}
                 />
     })
 
@@ -71,7 +75,7 @@ function Program() {
 
                             <span>
                                 <h3>Equity-free Funds</h3>
-                                <p>Each week, we bring in founders, investors, and industry experts to help guide our teams.</p>
+                                <p>We fund our teams to provide student startups with financial support for their project. We call them micro grants, made to alleviate students' financial concerns during the development of their startup with no strings attached.</p>
                             </span>
                         </article>
                         <article>
@@ -81,7 +85,7 @@ function Program() {
 
                             <span>
                                 <h3>Mentorship</h3>
-                                <p>Leveraging our network of industry partners, our program provides interactive sessions. </p>
+                                <p>We connect founders with a handful of thought-leaders (e.g. founders, investors, or executives) within each company's space to provide guidance and build a lasting relationship. Our Monday Mentor Sessions also provide feedback from 40+ individual mentors over the course of the batch. </p>
                             </span>
 
                         </article>
@@ -90,8 +94,8 @@ function Program() {
                                 <img src={workshops} alt="" />
                             </span>
                             <span>
-                                <h3>Workshops</h3>
-                                <p>We work with founders to hone their ability to pitch to venture capital and angel investors successfully. </p>
+                                <h3>Curriculum</h3>
+                                <p>Our workshops are lead by industry or academic experts and they are designed to provide information that tracks the development of each team's progress and premptively answers any questions that may arise. Topics range broadly and include design, product managment, funding, and legal incorporation. </p>
                             </span>
                         </article>
                         <article>
@@ -101,7 +105,7 @@ function Program() {
 
                             <span>
                                 <h3>Community</h3>
-                                <p>Each week, we bring in founders, investors, and industry experts to help guide our teams.</p>
+                                <p>Free Ventures provides students access to a variety of contacts from industry, academia, and the student community. This network can be tapped to provide technical expertise, funding advice, legal services, amongst other wisdom and help. Get plugged in to the network you need to succeed.</p>
                             </span>
                         </article>
                         <article>
@@ -111,7 +115,7 @@ function Program() {
 
                             <span>
                                 <h3>Legal/Banking Information</h3>
-                                <p>Leveraging our network of industry partners, our program provides interactive sessions. </p>
+                                <p>We partner with SVB and Cooley to provide deals on legal and banking assistance, which can range from opening a corporate banking account, incorporating your company, as well as the complexity of dividing up equity between founders. We host multiple sessions with Cooley and SVB to provide you with the best resources at the right time and at founder friendly costs.  </p>
                             </span>
 
                         </article>
@@ -121,7 +125,7 @@ function Program() {
                             </span>
                             <span>
                                 <h3>Fundraising Guidance</h3>
-                                <p>We work with founders to hone their ability to pitch to venture capital and angel investors successfully. </p>
+                                <p>At the end of each semester we organize VC trips with top firms for founders to pitch in front of notable investors. We also leverage our network of angel investors and pre-seed firms to connect our batch companies while they are in the process of fundraising. We help our founders refine their pitch to guarantee best results.  </p>
                             </span>
                         </article>
                         </div>
@@ -140,10 +144,10 @@ function Program() {
                             </div>
                             <div className="trackTeams">
                                 <h1>TEAMS</h1>
-                                <h3>over $30 million raised from:</h3>
+                                <h3>over $40 million raised from:</h3>
                                 <div className="teamsVCPics">
                                     <img src={yc}/>
-                                    <img src={greylock}/>
+                                    <img className= "greylock" src={greylock}/>
                                     <img src={kp}/>
                                     <img src={accel}/>
                                 </div>

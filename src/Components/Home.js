2240b9125718c11d795d648f551eb55982ee38ce
Adhiv Dhar <adhivd@gmail.com>
2019-08-26 19:54:03 -0700

    changes

--- src/Components/Home.js
+++ src/Components/Home.js
@@ -290,7 +290,7 @@ function Home() {
 
             </div>
         </div>
-        <svg xmlns="http://www.w3.org/2000/svg" className="blueWaveTop" viewBox="0 0 1440 320"><path fill="#f8ecfc" fill-opacity="1" d="M0,96L80,117.3C160,139,320,181,480,181.3C640,181,800,139,960,128C1120,117,1280,139,1360,149.3L1440,160L1440,320L1360,320C1280,320,1120,320,960,320C800,320,640,320,480,320C320,320,160,320,80,320L0,320Z"></path></svg>
+        <svg xmlns="http://www.w3.org/2000/svg" className="blueWaveTop" viewBox="0 0 1440 320"><path fill="#eeeeee" fill-opacity="1" d="M0,96L80,117.3C160,139,320,181,480,181.3C640,181,800,139,960,128C1120,117,1280,139,1360,149.3L1440,160L1440,320L1360,320C1280,320,1120,320,960,320C800,320,640,320,480,320C320,320,160,320,80,320L0,320Z"></path></svg>
         <div className="overview">
             <div className="container overviewLayout">
                 <div className="mini-header">
